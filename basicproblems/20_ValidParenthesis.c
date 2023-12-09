39cfe60067c7400c7678eb0760490d0049290c07
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2023-12-09 21:43:19 +0530

    Update 20_ValidParenthesis.c

--- basicproblems/20_ValidParenthesis.c
+++ basicproblems/20_ValidParenthesis.c
@@ -1,1 +1,25 @@
+bool isValid(char* s) {
 
+    int i,n=0;
+    while (s[n]!='\0'){
+        n++;
+    }
+    int top=0;
+    char* stack = (char*)malloc(n * sizeof(char));
+    for (int i=0;i<n;i++){
+        if (s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            stack[top]=s[i];
+            top++;
+        }
+        else if (top>0 && ((s[i] == ')' && stack[top - 1] == '(') ||
+                    (s[i] == ']' && stack[top - 1] == '[') ||
+                    (s[i] == '}' && stack[top - 1] == '{'))) 
+            top--;
+        else{
+            free(stack);
+            return false;
+        }
+    }
+    free(stack);
+    return top==0;
+}
