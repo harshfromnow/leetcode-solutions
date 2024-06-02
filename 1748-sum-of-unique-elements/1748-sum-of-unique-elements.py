0d18b8a912f990a1522af20563eba2f256439522
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2024-06-02 12:22:05 +0530

    Time: 43 ms (20.61%), Space: 16.4 MB (84.88%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,8 +1,12 @@
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
-        m=Counter(nums)
-        s=0
-        for k in m:
-            if m[k]==1:
-                s+=k
-        return s
+        hashmap = {}
+        for i in nums:
+            if i in hashmap.keys():
+                hashmap[i] += 1
+            else:
+                hashmap[i] = 1
+        sum = 0
+        for k, v in hashmap.items():
+            if v == 1: sum += k
+        return sum
