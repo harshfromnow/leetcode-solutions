71a8102decbedc4ad379139869d905f71d516fbc
harshfromnow <133039634+harshfromnow@users.noreply.github.com>
2024-06-04 19:28:13 +0530

    Time: 41 ms (26.4%), Space: 16.6 MB (34.5%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,17 +1,13 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        m={}
-        for k in s:
-            if k not in m:
-                m[k]=1
-            else:
-                m[k]+=1
-        c=0
+        c=set()
         l=0
-        for k in m:
-            if c==0 and m[k]==1:
-                l+=1
-                c=1
-            if m[k]%2==0:
-                l+=m[k]
+        for k in s:
+            if k in c:
+                c.remove(k)
+                l+=2
+            else:
+                c.add(k)
+        if c:
+            l+=1
         return l
